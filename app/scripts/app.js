855dbf95c4025d00cb8b2f96603e905fda800190
Andrew Hudson <ahudson@gmail.com>
2014-11-10 19:39:53 +0000

    add a cv page/view thing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/cv', {
+        templateUrl: 'views/cv.html',
+        controller: 'CVCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/cv.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name theOneAppApp.controller:CVCtrl
+ * @description
+ * # CVCtrl
+ * Controller of the theOneAppApp
+ */
+angular.module('theOneAppApp')
+  .controller('CVCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
