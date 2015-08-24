f3e01d274eaafbfe826c717fbd47368f65c01377
Andrew Hudson <ahudson@gmail.com>
2015-08-24 20:31:27 +0100

    remove old files before starting to use yo angular generator

--- test/post.xxx.js
+++
@@ -1,9 +0,0 @@
-describe('filter', function () {
-		beforeEach(module('app'));
-
-		describe('reverse', function() {
-				it('shoud reverse a string', inject(function (reverseFilter) {
-						expect(reverseFilter('ABCD')).toEqual('DCBA');
-				}));
-		});
-});
