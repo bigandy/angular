da93fcc4558e45f514c4025673580242f5a9f9dc
Andrew Hudson <ahudson@gmail.com>
2015-08-25 15:48:04 +0100

    add posts-auto to the mix

--- app/scripts/controllers/posts-auto.js
+++ app/scripts/controllers/posts-auto.js
@@ -4,29 +4,43 @@
  * @ngdoc function
  * @name wpapiApp.controller:PostsAutoCtrl
  * @description
- * # PostsAutoCtrl
+ * # PostsCtrl
  * Controller of the wpapiApp
  */
 var app = angular.module('wpapiApp');
 
 app.filter('to_trusted', ['$sce', function($sce){
-	return function(text) {
-		return $sce.trustAsHtml(text);
-	};
-}]);
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}]);
 
-  // Add a controller
+// Add a controller
 app.controller( 'PostsAutoCtrl', ['$scope', '$http', function( $scope, $http ) {
-  // Load posts from the WordPress API
-  $http({
-	cache: true,
-	method: 'GET',
-	url: 'https://big-andy.co.uk/wp-json/wp/v2/posts',
-	params: {
-	  'filter[posts_per_page]' : 10
+	var pendingTask;
+
+	function fetch() {
+		// Load posts from the WordPress API
+		$http({
+			cache: true,
+			method: 'GET',
+			url: 'https://big-andy.co.uk/wp-json/wp/v2/posts',
+			params: {
+				'filter[posts_per_page]' : $scope.ctrl.number
+			}
+		}).
+		success( function( data ) {
+			$scope.posts = data;
+		});
 	}
-  }).
-  success( function( data ) {
-	$scope.posts = data;
-  });
+
+	// Change the number of posts if the number in the input is changed
+	$scope.change = function() {
+		if (pendingTask) {
+			clearTimeout(pendingTask);
+		}
+		pendingTask = setTimeout(fetch(), 800);
+	};
+
+
 }]);
